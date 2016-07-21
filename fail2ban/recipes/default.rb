0181b07c86ad49212690104ba5287c2e0cd65696
Jorge Munoz <elgeorge2k@gmail.com>
2016-07-21 00:54:13 +0000

    Separate the enable/start from reload of service

--- fail2ban/recipes/default.rb
+++ fail2ban/recipes/default.rb
@@ -9,6 +9,9 @@
 package 'fail2ban' do
   action :install
 end
+service 'fail2ban' do
+  action [ :enable, :start ]
+end
 remote_directory "/etc/fail2ban/action.d" do
   source "action.d"
 end
@@ -22,5 +25,5 @@ cookbook_file "/etc/fail2ban/jail.local" do
   source "jail.local"
 end
 service 'fail2ban' do
-  action [ :enable, :start ]
+  action [ :reload ]
 end
